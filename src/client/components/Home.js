9c22cf05e2c8623c9e4515d216d1c9ac62a49b3b
Gaurav <gauravkr2506@gmail.com>
2020-11-20 17:14:57 +0530

    initial project setup

---
+++ src/client/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <div>I am Home xxcomponent</div>
+      <button onClick={() => console.log("hi there")}>Press me</button>
+    </div>
+  );
+}

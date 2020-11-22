45f3df653e355cbe92d1ccb13d98a214533ec117
Gaurav <gauravkr2506@gmail.com>
2020-11-22 22:31:29 +0530

    change port

--- src/index.js
+++ src/index.js
@@ -53,6 +53,6 @@ app.get("*", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(8080, () => {
+  console.log("listening on port 8080");
 });
